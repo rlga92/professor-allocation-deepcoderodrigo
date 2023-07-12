7c0c38837bbfc25254ab9891582792506102e25d
Rodrigo Lopes <rodrigolopesgurgel92@gmail.com>
2023-07-12 13:00:43 -0300

    Creating classes, attributes  and some annotations

---
+++ src/main/java/com/project/professor/allocation/entity/Allocation.java
@@ -0,0 +1,59 @@
+package com.project.professor.allocation.entity;
+
+import java.sql.Time;
+import java.time.DayOfWeek;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Allocation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private DayOfWeek day;
+	@Column(nullable = false)
+	private Time start;
+	@Column(nullable = false)
+	private Time end;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public DayOfWeek getDay() {
+		return day;
+	}
+
+	public void setDay(DayOfWeek day) {
+		this.day = day;
+	}
+
+	public Time getStart() {
+		return start;
+	}
+
+	public void setStart(Time start) {
+		this.start = start;
+	}
+
+	public Time getEnd() {
+		return end;
+	}
+
+	public void setEnd(Time end) {
+		this.end = end;
+	}
+
+}
---
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -0,0 +1,43 @@
+package com.project.professor.allocation.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Professor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true, nullable = false, length = 11)
+	private String cpf;
+	@Column(nullable = false)
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
